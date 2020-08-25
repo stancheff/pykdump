40f70bb6cae409dcfe4d80b8a54b13f4840a1f64
Alex Sidorenko <alexandre.sidorenko@hpe.com>
2020-08-25 15:01:18 -0400

    This program is called direclt from epython.c, to print common options processed by pykdump.API and removed before passing the to programs It is invoked via crash64> epython --ehelp

---
+++ Extension/epython_helper.py
@@ -0,0 +1,15 @@
+
+# A help subroutine to be called from epython.c to print some
+# values defined in pykdump.API
+
+# --------------------------------------------------------------------
+# (C) Copyright 2020 Hewlett Packard Enterprise Development LP
+#
+# Author: Alex Sidorenko <asid@hpe.com>
+#
+# --------------------------------------------------------------------
+
+from pykdump import API
+
+API.__epythonOptions(1)
+
